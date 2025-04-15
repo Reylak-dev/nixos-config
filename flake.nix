a82e219b8b2aa9be373190265be5c860b93ed361
Reylak <ANGULO18112008@gmail.com>
2025-04-15 16:56:54 -0300

    Structuring

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       specialArgs = {inherit inputs;};
       modules = [
-        ./configuration.nix
+        ./system/configuration.nix
         inputs.home-manager.nixosModules.default
       ];
     };
