88f4f4d3bc3a4946c2aa78b7669823a23b3517c4
Reylak <ANGULO18112008@gmail.com>
2025-05-31 17:41:21 -0300

    improvements on stylix

--- nixosModules/dotfiles/general/stylix.nix
+++ nixosModules/dotfiles/general/stylix.nix
@@ -12,14 +12,14 @@
       base03 = "424242";
       base04 = "9c9c9c";
       base05 = "9c9c9c";
-      base06 = "6005a6";
+      base06 = "8d3dfc";
       base07 = "ff6666";
       base08 = "ff0000";
       base09 = "fc4e4e";
       base0A = "ff0000";
       base0B = "8d3dfc";
       base0C = "808080";
-      base0D = "6005a6";
+      base0D = "8d3dfc";
       base0E = "4f4f4f";
       base0F = "43706f";
 
